da5b025af87fc5463036ff1cb3d4fc2738645991
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2025-07-28 21:30:38 -0300

    chore: initial commit

---
+++ backend/studora/src/main/java/com/robsonswe/studora/StudoraApplication.java
@@ -0,0 +1,13 @@
+package com.robsonswe.studora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudoraApplication.class, args);
+	}
+
+}
